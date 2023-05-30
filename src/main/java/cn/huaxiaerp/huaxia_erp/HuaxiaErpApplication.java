a120efb3a165cc1442b05b5e545658f156f21c77
Xiayutian <1445710086@qq.com>
2023-05-30 11:32:59 +0800

    Initial commit

---
+++ src/main/java/cn/huaxiaerp/huaxia_erp/HuaxiaErpApplication.java
@@ -0,0 +1,13 @@
+package cn.huaxiaerp.huaxia_erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaxiaErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuaxiaErpApplication.class, args);
+    }
+
+}
