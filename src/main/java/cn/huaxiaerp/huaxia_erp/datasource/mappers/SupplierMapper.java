5e55d4ce6be5ff04181b836f481b49e3b283bb6e
啊帽 <45966123@qq.com>
2023-05-30 16:50:28 +0800

    动态sql获取供应商信息

--- src/main/java/cn/huaxiaerp/huaxia_erp/datasource/mappers/SupplierMapper.java
+++ src/main/java/cn/huaxiaerp/huaxia_erp/datasource/mappers/SupplierMapper.java
@@ -7,7 +7,14 @@ import java.util.List;
 
 public interface SupplierMapper {
 
-
+    /**
+     * 动态sql获取供应商信息
+     * @param supplier 供应商名称
+     * @param telephone 手机号
+     * @param phoneNumber 联系电话
+     * @return
+     */
+    List<Supplier> getSupplierLikeSupplierOrTelephoneOrPhoneNumber(String supplier,String telephone,String phoneNumber);
     int deleteByPrimaryKey(Long id);
 
     int insert(Supplier record);
