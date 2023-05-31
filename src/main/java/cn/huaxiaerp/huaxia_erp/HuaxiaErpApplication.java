b39ca788eecbd2c787a0c11122723502c4f39013
LAPTOP-UQ6DCSAI\MI <123456>
2023-05-31 14:17:20 +0800

    Springboot扫描mapper层

--- src/main/java/cn/huaxiaerp/huaxia_erp/HuaxiaErpApplication.java
+++ src/main/java/cn/huaxiaerp/huaxia_erp/HuaxiaErpApplication.java
@@ -1,9 +1,11 @@
 package cn.huaxiaerp.huaxia_erp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.huaxiaerp.huaxia_erp.datasource.mappers")
 public class HuaxiaErpApplication {
 
     public static void main(String[] args) {
